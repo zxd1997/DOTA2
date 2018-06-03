3b7533c5d0f9ddc05d60e38a1a8642e03c14408d
zxd1997 <zxd1997@oulook.com>
2018-06-03 23:01:25 +0800

    add chart

--- app/src/main/java/com/example/zxd1997/dota2/Fragments/EconomyFragment.java
+++ app/src/main/java/com/example/zxd1997/dota2/Fragments/EconomyFragment.java
@@ -2,7 +2,6 @@ package com.example.zxd1997.dota2.Fragments;
 
 
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -19,7 +18,9 @@ import com.example.zxd1997.dota2.Utils.MyApplication;
 import java.util.ArrayList;
 import java.util.List;
 
+import lecho.lib.hellocharts.model.Axis;
 import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.LineChartData;
 import lecho.lib.hellocharts.model.PointValue;
 import lecho.lib.hellocharts.view.LineChartView;
 
@@ -56,18 +57,49 @@ public class EconomyFragment extends Fragment {
             Intent intent = new Intent(MyApplication.getContext(), MainActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
             getActivity().startActivity(intent);
-            getActivity().finish();
+            getActivity().finish(); 
         } else {
             LineChartView chart = view.findViewById(R.id.chart);
             List<Line> lines = new ArrayList<>();
             List<PointValue> pointValues = new ArrayList<>();
             int j = 0;
             for (int i : match.getRadiant_gold_adv()) {
-                pointValues.add(new PointValue(j++, i));
+                pointValues.add(new PointValue(j++, i).setLabel(i >= 0 ? "Radiant Gold Advantage:" + i : "Dire Gold Advantage:" + Math.abs(i)));
             }
             Line line = new Line(pointValues);
-            line.setColor(Color.GRAY);
+            line.setColor(getContext().getResources().getColor(R.color.very_high));
             line.setCubic(true);
+            line.setFilled(false);
+            line.setStrokeWidth(1);
+            line.setHasPoints(true);
+//            line.setHasLabels(true);
+            line.setHasLabelsOnlyForSelected(true);
+            line.setPointRadius(2);
+            lines.add(line);
+            pointValues = new ArrayList<>();
+            j = 0;
+            for (int i : match.getRadiant_xp_adv()) {
+                pointValues.add(new PointValue(j++, i).setLabel(i >= 0 ? "Radiant XP Advantage:" + i : "Dire XP Advantage:" + Math.abs(i)));
+            }
+            line = new Line(pointValues);
+            line.setColor(getContext().getResources().getColor(R.color.win));
+            line.setCubic(true);
+            line.setFilled(false);
+            line.setHasPoints(true);
+//            line.setHasLabels(true);
+            line.setHasLabelsOnlyForSelected(true);
+            line.setPointRadius(2);
+            line.setStrokeWidth(1);
+            lines.add(line);
+            LineChartData data = new LineChartData();
+            data.setLines(lines);
+            Axis axisY = new Axis().setName("Advantage").setHasLines(true);
+            data.setAxisYLeft(axisY);
+            Axis axisX = new Axis().setName("Time/min");
+            data.setAxisXBottom(axisX);
+            chart.setZoomEnabled(false);
+            chart.setInteractive(true);
+            chart.setLineChartData(data);
         }
         return view;
     }
