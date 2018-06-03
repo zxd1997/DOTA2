5ed9e26b4cb06dc159d08f9bbf94cbb709f5991f
zxd1997 <zxd1997@outlook.com>
2018-06-03 22:03:36 +0800

    chart

--- app/src/main/java/com/example/zxd1997/dota2/Fragments/EconomyFragment.java
+++ app/src/main/java/com/example/zxd1997/dota2/Fragments/EconomyFragment.java
@@ -2,6 +2,7 @@ package com.example.zxd1997.dota2.Fragments;
 
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -14,6 +15,13 @@ import com.example.zxd1997.dota2.Activities.MatchActivity;
 import com.example.zxd1997.dota2.Beans.Match;
 import com.example.zxd1997.dota2.R;
 import com.example.zxd1997.dota2.Utils.MyApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.Line;
+import lecho.lib.hellocharts.model.PointValue;
+import lecho.lib.hellocharts.view.LineChartView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -41,6 +49,7 @@ public class EconomyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         MatchActivity activity = (MatchActivity) getActivity();
+        View view = inflater.inflate(R.layout.fragment_economy, container, false);
         match = activity.getMatch();
         if (match == null || match.getPlayers() == null) {
             Log.d("null", "onCreateView: " + 111111);
@@ -49,9 +58,18 @@ public class EconomyFragment extends Fragment {
             getActivity().startActivity(intent);
             getActivity().finish();
         } else {
-
+            LineChartView chart = view.findViewById(R.id.chart);
+            List<Line> lines = new ArrayList<>();
+            List<PointValue> pointValues = new ArrayList<>();
+            int j = 0;
+            for (int i : match.getRadiant_gold_adv()) {
+                pointValues.add(new PointValue(j++, i));
+            }
+            Line line = new Line(pointValues);
+            line.setColor(Color.GRAY);
+            line.setCubic(true);
         }
-        return inflater.inflate(R.layout.fragment_economy, container, false);
+        return view;
     }
 
 }
