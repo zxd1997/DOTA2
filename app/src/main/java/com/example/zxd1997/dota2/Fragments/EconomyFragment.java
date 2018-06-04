34b50237132b213f3b5187aa1787e5d459d1b661
zxd1997 <zxd1997@outlook.com>
2018-06-04 15:47:52 +0800

    update chart touch and label draw

--- app/src/main/java/com/example/zxd1997/dota2/Fragments/EconomyFragment.java
+++ app/src/main/java/com/example/zxd1997/dota2/Fragments/EconomyFragment.java
@@ -12,17 +12,19 @@ import android.view.ViewGroup;
 import com.example.zxd1997.dota2.Activities.MainActivity;
 import com.example.zxd1997.dota2.Activities.MatchActivity;
 import com.example.zxd1997.dota2.Beans.Match;
+import com.example.zxd1997.dota2.Chart.MyLineChartRenderer;
+import com.example.zxd1997.dota2.Chart.MyLineChartView;
 import com.example.zxd1997.dota2.R;
 import com.example.zxd1997.dota2.Utils.MyApplication;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import lecho.lib.hellocharts.listener.LineChartOnValueSelectListener;
 import lecho.lib.hellocharts.model.Axis;
 import lecho.lib.hellocharts.model.Line;
 import lecho.lib.hellocharts.model.LineChartData;
 import lecho.lib.hellocharts.model.PointValue;
-import lecho.lib.hellocharts.view.LineChartView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,37 +61,36 @@ public class EconomyFragment extends Fragment {
             getActivity().startActivity(intent);
             getActivity().finish(); 
         } else {
-            LineChartView chart = view.findViewById(R.id.chart);
+            MyLineChartView chart = view.findViewById(R.id.chart);
+            chart.setChartRenderer(new MyLineChartRenderer(getContext(), chart, chart));
             List<Line> lines = new ArrayList<>();
             List<PointValue> pointValues = new ArrayList<>();
             int j = 0;
             for (int i : match.getRadiant_gold_adv()) {
                 pointValues.add(new PointValue(j++, i).setLabel(i >= 0 ? "Radiant Gold Advantage:" + i : "Dire Gold Advantage:" + Math.abs(i)));
             }
-            Line line = new Line(pointValues);
-            line.setColor(getContext().getResources().getColor(R.color.very_high));
-            line.setCubic(true);
-            line.setFilled(false);
-            line.setStrokeWidth(1);
-            line.setHasPoints(true);
-//            line.setHasLabels(true);
-            line.setHasLabelsOnlyForSelected(true);
-            line.setPointRadius(2);
+            Line line = new Line(pointValues)
+                    .setColor(getContext().getResources().getColor(R.color.very_high))
+                    .setCubic(true)
+                    .setFilled(false)
+                    .setStrokeWidth(1)
+                    .setHasPoints(true)
+                    .setHasLabelsOnlyForSelected(true)
+                    .setPointRadius(2);
             lines.add(line);
             pointValues = new ArrayList<>();
             j = 0;
             for (int i : match.getRadiant_xp_adv()) {
                 pointValues.add(new PointValue(j++, i).setLabel(i >= 0 ? "Radiant XP Advantage:" + i : "Dire XP Advantage:" + Math.abs(i)));
             }
-            line = new Line(pointValues);
-            line.setColor(getContext().getResources().getColor(R.color.win));
-            line.setCubic(true);
-            line.setFilled(false);
-            line.setHasPoints(true);
-//            line.setHasLabels(true);
-            line.setHasLabelsOnlyForSelected(true);
-            line.setPointRadius(2);
-            line.setStrokeWidth(1);
+            line = new Line(pointValues)
+                    .setColor(getContext().getResources().getColor(R.color.win))
+                    .setCubic(true)
+                    .setFilled(false)
+                    .setHasPoints(true)
+                    .setHasLabelsOnlyForSelected(true)
+                    .setPointRadius(2)
+                    .setStrokeWidth(1);
             lines.add(line);
             LineChartData data = new LineChartData();
             data.setLines(lines);
@@ -99,6 +100,17 @@ public class EconomyFragment extends Fragment {
             data.setAxisXBottom(axisX);
             chart.setZoomEnabled(false);
             chart.setInteractive(true);
+            chart.setOnValueTouchListener(new LineChartOnValueSelectListener() {
+                @Override
+                public void onValueSelected(int lineIndex, int pointIndex, PointValue value) {
+
+                }
+
+                @Override
+                public void onValueDeselected() {
+
+                }
+            });
             chart.setLineChartData(data);
         }
         return view;
--- app/src/main/java/com/example/zxd1997/dota2/Fragments/MyFragment.java
+++ app/src/main/java/com/example/zxd1997/dota2/Fragments/MyFragment.java
@@ -21,6 +21,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
@@ -47,14 +48,14 @@ public class MyFragment extends Fragment {
     LinearLayout my;
     ProgressBar progressBar;
     SharedPreferences sharedPreferences;
-    String id="";
+    static final int VERIFY = 0;
     String tmp;
     Button button1;
     Button button;
-    static final int VERIFY=0;
-    static final int PLAYER_INFO=1;
-    static final int WL=2;
-    static final int RECENT_MATCHES=3;
+    static final int PLAYER_INFO = 1;
+    static final int WL = 2;
+    static final int RECENT_MATCHES = 3;
+    String id = "";
     Player player;
     final String DISCONNECT = "disconnect from id";
     SimpleDraweeView header;
@@ -73,86 +74,89 @@ public class MyFragment extends Fragment {
     SwipeRefreshLayout swipeRefreshLayout;
     TextInputLayout textInputLayout;
     com.example.zxd1997.dota2.Beans.WL wl;
-    List<RecentMatch> recentMatches=new ArrayList<>();
+    List<RecentMatch> recentMatches = new ArrayList<>();
     Receiver receiver;
-    Handler handler=new Handler(){
+    Handler handler = new Handler() {
         @Override
         public void handleMessage(Message message) {
-            Log.d("id", "handleMessage: "+message.obj.toString());
-            switch (message.what){
-                case VERIFY:{
-                    Player t=new Gson().fromJson(message.obj.toString(),Player.class);
+            Log.d("id", "handleMessage: " + message.obj.toString());
+            switch (message.what) {
+                case VERIFY: {
+                    Player t = new Gson().fromJson(message.obj.toString(), Player.class);
                     if (t == null) {
-                        Toast.makeText(getContext(),"No Such Player",Toast.LENGTH_LONG).show();
+                        Toast.makeText(getContext(), "No Such Player", Toast.LENGTH_LONG).show();
                         progressBar.setVisibility(View.GONE);
                         button1.setVisibility(View.VISIBLE);
-                    }else{
+                    } else {
                         linearLayout.setVisibility(View.GONE);
-                        SharedPreferences.Editor editor=sharedPreferences.edit();
-                        id=tmp;
-                        editor.putString("id",id);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        id = tmp;
+                        editor.putString("id", id);
                         editor.commit();
-                        Okhttp.getFromService(getString(R.string.api)+getString(R.string.players)+id,handler,PLAYER_INFO);
+                        Okhttp.getFromService(getString(R.string.api) + getString(R.string.players) + id, handler, PLAYER_INFO);
                     }
                     break;
                 }
-                case PLAYER_INFO:{
-                    player=new Gson().fromJson(message.obj.toString(),Player.class);
+                case PLAYER_INFO: {
+                    player = new Gson().fromJson(message.obj.toString(), Player.class);
                     personaname.setText(player.getPersonaname());
                     loccountrycode.setText(player.getLoccountrycode());
                     account_id.setText(String.valueOf(player.getAccount_id()));
                     header.setImageURI(player.getAvatarfull());
-                    int t=(int)Math.floor((double)player.getRank_tier()/10);
-                    if (player.getRank_tier()!=0){
-                        int star=player.getRank_tier()%10;
-                        int rank=player.getLeaderboard_rank();
-                        Log.d("rank", "handleMessage: "+rank);
-                        if (rank>0){
-                            if (rank<=1000){
-                                t=8;star=0;
+                    int t = (int) Math.floor((double) player.getRank_tier() / 10);
+                    if (player.getRank_tier() != 0) {
+                        int star = player.getRank_tier() % 10;
+                        int rank = player.getLeaderboard_rank();
+                        Log.d("rank", "handleMessage: " + rank);
+                        if (rank > 0) {
+                            if (rank <= 1000) {
+                                t = 8;
+                                star = 0;
                             }
-                            if (rank<=100){
-                                t=9;star=0;
+                            if (rank <= 100) {
+                                t = 9;
+                                star = 0;
                             }
-                            if (rank<=10){
-                                t=10;star=0;
+                            if (rank <= 10) {
+                                t = 10;
+                                star = 0;
                             }
-                            ranks.setText(rank+"");
+                            ranks.setText(rank + "");
                         }
-                        if (star>0){
-                            TypedArray typedArray=getContext().getResources().obtainTypedArray(R.array.stars);
-                            stars.setImageResource(typedArray.getResourceId(star-1,0));
+                        if (star > 0) {
+                            TypedArray typedArray = getContext().getResources().obtainTypedArray(R.array.stars);
+                            stars.setImageResource(typedArray.getResourceId(star - 1, 0));
                         }
                     }
-                    Log.d("rank", "handleMessage: "+t);
-                    TypedArray typedArray=getContext().getResources().obtainTypedArray(R.array.tiers);
-                    tier.setImageResource(typedArray.getResourceId(t,0));
-                    Okhttp.getFromService(getString(R.string.api)+getString(R.string.players)+id+"/wl",handler,WL);
+                    Log.d("rank", "handleMessage: " + t);
+                    TypedArray typedArray = getContext().getResources().obtainTypedArray(R.array.tiers);
+                    tier.setImageResource(typedArray.getResourceId(t, 0));
+                    Okhttp.getFromService(getString(R.string.api) + getString(R.string.players) + id + "/wl", handler, WL);
                     break;
                 }
-                case WL:{
-                    wl=new Gson().fromJson(message.obj.toString(),WL.class);
+                case WL: {
+                    wl = new Gson().fromJson(message.obj.toString(), WL.class);
                     wl.setWinrate();
-                    java.text.DecimalFormat df =new java.text.DecimalFormat("#.00%");
-                    String t=df.format(wl.getWinrate());
-                    win.setText(getString(R.string.win)+":"+wl.getWin());
-                    lose.setText(getString(R.string.lose)+":"+wl.getLose());
-                    winrate.setText(getString(R.string.winrate)+":"+t);
-                    Okhttp.getFromService(getString(R.string.api)+getString(R.string.players)+id+"/recentMatches",handler,RECENT_MATCHES);
+                    java.text.DecimalFormat df = new java.text.DecimalFormat("#.00%");
+                    String t = df.format(wl.getWinrate());
+                    win.setText(getString(R.string.win) + ":" + wl.getWin());
+                    lose.setText(getString(R.string.lose) + ":" + wl.getLose());
+                    winrate.setText(getString(R.string.winrate) + ":" + t);
+                    Okhttp.getFromService(getString(R.string.api) + getString(R.string.players) + id + "/recentMatches", handler, RECENT_MATCHES);
                     my.setVisibility(View.VISIBLE);
                     swipeRefreshLayout.setVisibility(View.GONE);
                     break;
                 }
-                case RECENT_MATCHES:{
+                case RECENT_MATCHES: {
                     recentMatches.clear();
-                    JsonParser parser=new JsonParser();
-                    JsonArray jsonArray=parser.parse(message.obj.toString()).getAsJsonArray();
-                    for (JsonElement e:jsonArray){
-                        RecentMatch recentMatch=new Gson().fromJson(e,RecentMatch.class);
+                    JsonParser parser = new JsonParser();
+                    JsonArray jsonArray = parser.parse(message.obj.toString()).getAsJsonArray();
+                    for (JsonElement e : jsonArray) {
+                        RecentMatch recentMatch = new Gson().fromJson(e, RecentMatch.class);
                         recentMatches.add(recentMatch);
                     }
-                    for (RecentMatch i:recentMatches){
-                        Log.d("id", "handleMessage: "+i.getMatch_id());
+                    for (RecentMatch i : recentMatches) {
+                        Log.d("id", "handleMessage: " + i.getMatch_id());
                     }
                     progressBar.setVisibility(View.GONE);
                     swipeRefreshLayout.setVisibility(View.VISIBLE);
@@ -164,6 +168,7 @@ public class MyFragment extends Fragment {
 
         }
     };
+
     public MyFragment() {
     }
 
@@ -180,35 +185,35 @@ public class MyFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        final View view=inflater.inflate(R.layout.fragment_my, container, false);
-        linearLayout=view.findViewById(R.id.no_binding);
-        sharedPreferences= PreferenceManager.getDefaultSharedPreferences(getContext());
-        my=view.findViewById(R.id.my);
-        button1=view.findViewById(R.id.verify);
-        progressBar=view.findViewById(R.id.progressBar);
-        header=view.findViewById(R.id.header);
-        personaname=view.findViewById(R.id.personaname);
-        loccountrycode=view.findViewById(R.id.loccountrycode);
-        account_id=view.findViewById(R.id.account_id);
-        win=view.findViewById(R.id.win);
-        lose=view.findViewById(R.id.lose);
-        winrate=view.findViewById(R.id.win_rate);
-        tier=view.findViewById(R.id.tier);
-        ranks=view.findViewById(R.id.rank);
-        stars=view.findViewById(R.id.star);
-        recyclerView=view.findViewById(R.id.recent_matches);
+        final View view = inflater.inflate(R.layout.fragment_my, container, false);
+        linearLayout = view.findViewById(R.id.no_binding);
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+        my = view.findViewById(R.id.my);
+        button1 = view.findViewById(R.id.verify);
+        progressBar = view.findViewById(R.id.progressBar);
+        header = view.findViewById(R.id.header);
+        personaname = view.findViewById(R.id.personaname);
+        loccountrycode = view.findViewById(R.id.loccountrycode);
+        account_id = view.findViewById(R.id.account_id);
+        win = view.findViewById(R.id.win);
+        lose = view.findViewById(R.id.lose);
+        winrate = view.findViewById(R.id.win_rate);
+        tier = view.findViewById(R.id.tier);
+        ranks = view.findViewById(R.id.rank);
+        stars = view.findViewById(R.id.star);
+        recyclerView = view.findViewById(R.id.recent_matches);
         button = view.findViewById(R.id.connect);
         textInputLayout = view.findViewById(R.id.text_input);
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
-        matchesAdapter=new MatchesAdapter(getContext(),recentMatches);
+        matchesAdapter = new MatchesAdapter(getContext(), recentMatches);
         recyclerView.setAdapter(matchesAdapter);
-        swipeRefreshLayout=view.findViewById(R.id.swipe);
+        swipeRefreshLayout = view.findViewById(R.id.swipe);
         textView = view.findViewById(R.id.text_notice);
         swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
                 swipeRefreshLayout.setRefreshing(true);
-                Okhttp.getFromService(getString(R.string.api)+getString(R.string.players)+id,handler,PLAYER_INFO);
+                Okhttp.getFromService(getString(R.string.api) + getString(R.string.players) + id, handler, PLAYER_INFO);
             }
         });
         button1.setOnClickListener(new View.OnClickListener() {
@@ -218,6 +223,10 @@ public class MyFragment extends Fragment {
                 if (textInputEditText.getText().toString().equals("")) {
                     Toast.makeText(getContext(), "Please input your Steam 32 ID!", Toast.LENGTH_LONG).show();
                 } else {
+                    InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                    if (imm.isActive()) {
+                        imm.hideSoftInputFromWindow(v.getApplicationWindowToken(), 0);
+                    }
                     tmp = textInputEditText.getText().toString();
                     Okhttp.getFromService(getString(R.string.api) + getString(R.string.players) + tmp, handler, VERIFY);
                     button1.setVisibility(View.GONE);
@@ -225,7 +234,7 @@ public class MyFragment extends Fragment {
                 }
             }
         });
-        if (sharedPreferences.getString("id","").equals("")){
+        if (sharedPreferences.getString("id", "").equals("")) {
             linearLayout.setVisibility(View.VISIBLE);
             my.setVisibility(View.GONE);
 
@@ -238,12 +247,12 @@ public class MyFragment extends Fragment {
                     button1.setVisibility(View.VISIBLE);
                 }
             });
-        }else{
-            id=sharedPreferences.getString("id","");
+        } else {
+            id = sharedPreferences.getString("id", "");
         }
-        if (!id.equals("")){
+        if (!id.equals("")) {
             progressBar.setVisibility(View.VISIBLE);
-            Okhttp.getFromService(getString(R.string.api)+getString(R.string.players)+id,handler,PLAYER_INFO);
+            Okhttp.getFromService(getString(R.string.api) + getString(R.string.players) + id, handler, PLAYER_INFO);
         }
         IntentFilter intentFilter = new IntentFilter(DISCONNECT);
         receiver = new Receiver();
